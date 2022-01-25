6253b7258e22255f8d5dbf7edaafb7e0a7c0fda1
Eder Chiquito <eder.almeida40@gmail.com>
2022-01-25 19:55:59 -0300

    added connection to graphql

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,28 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import {
+  ApolloClient,
+  ApolloProvider,
+  gql,
+  InMemoryCache,
+} from '@apollo/client';
 import Routes from './routes';
 
+const client = new ApolloClient({
+  uri: 'http://192.168.100.16:3333/graphql',
+  cache: new InMemoryCache(),
+});
+
 const App: React.FC = () => (
-  <NavigationContainer>
-    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
-      <Routes />
-    </View>
-  </NavigationContainer>
+  <ApolloProvider client={client}>
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+        <Routes />
+      </View>
+    </NavigationContainer>
+  </ApolloProvider>
 );
 
 export default App;
--- src/pages/Characters/index.tsx
+++ src/pages/Characters/index.tsx
@@ -1,10 +1,44 @@
+import { gql, useQuery } from '@apollo/client';
 import React from 'react';
 import { Text, View } from 'react-native';
 
+interface IClass {
+  id: string;
+  name: string;
+  description: string;
+  hitDice: string;
+  primaryAbility: string;
+}
+
+interface IClassData {
+  pcClass: IClass;
+}
+
+const bard = gql`
+  query GetClassById {
+    pcClass(id: "437fbeea-d481-4333-8b5a-471f8d0005fc") {
+      name
+      hitDice
+      primaryAbility
+    }
+  }
+`;
+
 const Characters: React.FC = () => {
+  const { loading, error, data } = useQuery<IClassData>(bard);
+  if (loading) return <Text>Loading</Text>;
+  if (error) return <Text>Error :(</Text>;
   return (
     <View>
-      <Text>Character page</Text>
+      <Text>Classe Bardo</Text>
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <>
+          <Text>Dados</Text>
+          {data && <Text>{data.pcClass.description}</Text>}
+        </>
+      )}
     </View>
   );
 };
