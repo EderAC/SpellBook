be15decd075a84a548b9394747af51de1a526700
Eder Chiquito <eder.almeida40@gmail.com>
2022-01-25 18:30:18 -0300

    created app router

---
+++ src/pages/Characters/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Characters: React.FC = () => {
+  return (
+    <View>
+      <Text>Character page</Text>
+    </View>
+  );
+};
+
+export default Characters;
